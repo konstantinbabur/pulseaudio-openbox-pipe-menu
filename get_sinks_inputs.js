ad10789cede06decbaddf2797da4aafe49c35ae5
Konstantin Babur <konstantin.babur@gmail.com>
2018-09-17 03:34:20 +0300

    добавил список аудио выходов по умолчанию

--- get_sinks_inputs.js
+++ get_sinks_inputs.js
@@ -10,72 +10,87 @@ const streamDataPromisify = (stream, encoding) => {
   });
 };
 
-const listSinkInputsPromise = 
-  streamDataPromisify(spawn('pacmd', ['list-sink-inputs']).stdout, 'utf-8');
-
-const listSinksPromise = 
-  streamDataPromisify(spawn('pacmd', ['list-sinks']).stdout, 'utf-8');
-
 const inputSinkRegexp = /((index):\s*(\d+)|(sink):\s*(\d+)|(media\.name)\s*=\s*"(.+)"|(application\.name)\s*=\s*"(.+)")/g;
-const sinkRegexp = /((index):\s*(\d+)|(device\.description)\s*=\s*"(.+)")/g;
+const sinkRegexp = /((\*)?\s*(index):\s*(\d+)|(device\.description)\s*=\s*"(.+)")/g;
 
-Promise.all([listSinkInputsPromise.then(data => {
-  let match,
-    current;
-  const inputsSinks = [];
-  while (match = inputSinkRegexp.exec(data)) {
-    if ([match[2]] == 'index') {
-      inputsSinks.push(current = {index: match[3]});
+Promise.all([
+  streamDataPromisify(spawn('pacmd', ['list-sink-inputs']).stdout, 'utf-8').then(data => {
+    let match,
+      current;
+    const inputsSinks = [];
+    while (match = inputSinkRegexp.exec(data)) {
+      if (match[2] == 'index') {
+        inputsSinks.push(current = {index: match[3]});
+      }
+      else if (match[4] == 'sink') {
+        current.sink = match[5];
+      }
+      else if (match[8] == 'application.name') {
+        current.application = match[9];
+      }
+      else if (match[6] == 'media.name') {
+        current.media = match[7];
+      }
     }
-    else if (match[4] == 'sink') {
-      current.sink = match[5];
+    return {inputsSinks};
+  }),
+  streamDataPromisify(spawn('pacmd', ['list-sinks']).stdout, 'utf-8').then(data => {
+    let match,
+      current;
+    const sinks = [];
+    while (match = sinkRegexp.exec(data)) {
+      if (match[3] == 'index') {
+        sinks.push(current = {
+          index: match[4], 
+          default: match[2] == '*'
+        });
+      }
+      else if (match[5] == 'device.description') {
+        current.name = match[6];
+      }
     }
-    else if (match[8] == 'application.name') {
-      current.application = match[9];
-    }
-    else if (match[6] == 'media.name') {
-      current.media = match[7];
-    }
-  }
-  return {inputsSinks};
-}),
-listSinksPromise.then(data => {
-  let match,
-    current;
-  const sinks = [];
-  while (match = sinkRegexp.exec(data)) {
-    if ([match[2]] == 'index') {
-      sinks.push(current = {index: match[3]});
-    }
-    else if (match[4] == 'device.description') {
-      current.name = match[5];
-    }
-  }
-  return {sinks};
-})]).then(requestResults => {
-  const sinksAndInputs = {};
-  requestResults.forEach(requestResult => Object.assign(sinksAndInputs, requestResult));
-  process.stdout.write('<openbox_pipe_menu>');
+    return {sinks};
+  })
+]).then(requestResults => {
+  const sinksAndInputs = requestResults.reduce(
+      (accamulator, requestResult) => 
+        Object.assign(accamulator, requestResult), {}
+    ),
+    stdout = process.stdout;
+
+  stdout.write('<openbox_pipe_menu>');
+  stdout.write('<separator label="аудио потоки"/>');
+
   sinksAndInputs.inputsSinks.forEach(input => {
-    let menuArray = [
-      '<menu id="pacmd-input-', input.index,'" label="',
-      input.index,
-      ' ', input.application, ':', input.media,
-      '">'
-    ];
+    const inputIndex = input.index;
+
+    stdout.write(`<menu id="pacmd-input-${inputIndex}" label="${inputIndex} ${input.application}:${input.media}">`);
+    stdout.write('<separator label="устройства"/>');
+
     sinksAndInputs.sinks.forEach(sink => {
-      Array.prototype.push.apply(menuArray, [
-        '<item label="', (sink.index == input.sink ? '*' : ' ') ,' ', sink.index, ' ', sink.name, '">',
-        '<action name="Execute">',
-        '<command>pacmd move-sink-input ', input.index, ' ', sink.index, '</command>',
-        '</action>',
-        '</item>'
-      ]);
+      stdout.write(`
+        <item label="${sink.index == input.sink ? '*' : ' '} ${sink.index} ${sink.name}">
+          <action name="Execute">
+            <command>pacmd move-sink-input ${inputIndex} ${sink.index}</command>
+          </action>
+        </item>
+      `);
     });
-    Array.prototype.push.apply(menuArray, [
-      '</menu>'
-    ]);
-    process.stdout.write(menuArray.join(''));
+
+    stdout.write('</menu>');
   });
-  process.stdout.write('</openbox_pipe_menu>');
+
+  stdout.write('<separator label="выход по-умолчанию"/>');
+
+  for (let sink of sinksAndInputs.sinks) {
+    stdout.write(`
+        <item label="${sink.default === true ? '*' : ' '} ${sink.index} ${sink.name}">
+          <action name="Execute">
+            <command>pacmd set-default-sink ${sink.index}</command>
+          </action>
+        </item>
+    `);
+  }
+
+  stdout.write('</openbox_pipe_menu>');
 });
